fadf823587232d2f9853dcedd24c571bad9d9f4f
amp <am.perescu@gmail.com>
2020-09-29 17:50:26 +0300

    Initial Commit

---
+++ Mindji/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mindji
+//
+//  Created by Antonela Madalina on 29/09/2020.
+//  Copyright © 2020 am.p. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
